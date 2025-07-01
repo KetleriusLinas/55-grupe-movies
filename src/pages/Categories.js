aeea6bb33ddac3bdafca66e155ffc1cd2ba06532
Linas <ketleriuslinas@gmail.com>
2025-07-01 18:11:19 +0300

    categories list

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,19 +1,14 @@
-import { placeholder } from "../ui/placeholder.js";
+import { categoriesData } from "../data/categoriesData.js";
 import { PageTemplate } from "../templates/PageTemplate.js";
+import { categoriesListSection } from "../ui/categoriesList.js";
 import { pageTitle } from "../ui/pageTitle.js";
 
 export class PageCategories extends PageTemplate {
     main() {
         return `
-            <main> 
-                ${pageTitle('Catergory')}
-                ${placeholder('Catergory list', [
-            'pavadinimas',
-            'trumpas aprasas',
-            'filmu kiekis toje kategorijoje',
-            'nuoroda i vidini puslapi',
-            'pavadinimas',
-        ])}
+            <main>
+                ${pageTitle('Categories')}
+                ${categoriesListSection(categoriesData)}
             </main>`;
     }
 }
