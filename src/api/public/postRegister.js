f42e4d647f14856b680bcadbfec5d6a257e974a9
Linas <ketleriuslinas@gmail.com>
2025-07-06 20:55:56 +0300

    DB

--- src/api/public/postRegister.js
+++ src/api/public/postRegister.js
@@ -1,13 +1,12 @@
+import { connection } from "../../db.js";
 import { IsValid } from "../../lib/IsValid.js";
 
-    
-export function postRegister(req, res) {
+export async function postRegister(req, res) {
     const [err, msg] = IsValid.fields(req.body, {
         username: 'username',
         email: 'email',
         password: 'password',
         tos: 'tos',
-        
     });
 
     if (err) {
@@ -15,6 +14,17 @@ export function postRegister(req, res) {
             status: 'error',
             msg: msg,
         });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+        const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?);`;
+        const response = await connection.execute(sql, [username, email, password]);
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
     }
 
     return res.json({
