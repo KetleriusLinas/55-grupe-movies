577e6163e13182955d255788c5f4c3675a6e59a0
Linas <ketleriuslinas@gmail.com>
2025-07-15 14:37:02 +0300

    delete button

--- public/js/category-list.js
+++ public/js/category-list.js
@@ -1,16 +1,18 @@
-const deleteButtonDOM = document.querySelectorAll('table button');
+const deleteButtonsDOM = document.querySelectorAll('table button');
 
-for (const btnDOM of deleteButtonDOM) {
+for (const btnDOM of deleteButtonsDOM) {
     btnDOM.addEventListener('click', () => {
         console.log('delete', btnDOM.dataset.url);
 
-        fetch('api/admin/categories/' + btnDOM.dataset.url, {
+        fetch('/api/admin/categories/' + btnDOM.dataset.url, {
             method: 'DELETE',
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-
+                if (data.status === 'success') {
+                    // btnDOM.parentElement.parentElement.remove();
+                    btnDOM.closest('tr').remove();
+                }
             })
             .catch(console.error);
     });
